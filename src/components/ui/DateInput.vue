6c0fd4b0ffd8e90d3752ea80a8da3d3734be5f12
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2024-05-19 20:27:36 +0500

    Update DateInput.vue

--- src/components/ui/DateInput.vue
+++ src/components/ui/DateInput.vue
@@ -108,43 +108,23 @@ onMounted(() => {
   const locale = navigator.language || 'en-US'
   mask.value = locale === 'en-US' ? 'MM/DD/YYYY' : 'DD/MM/YYYY'
 })
-
 const minDate = dayjs('1900-01-01', 'YYYY-MM-DD')
 const maxDate = dayjs(new Date().getFullYear() + '-12-31', 'YYYY-MM-DD')
-
 const format = (date: string) => {
   const parsedDate = dayjs(date, 'YYYY-MM-DD')
   return parsedDate.isValid() ? parsedDate.format('YYYY/MM/DD') : ''
 }
-
+const regEx = mask.value === 'en-US' ? /^(\d{2})(\d{2})(\d{4})$/ : /^(\d{2})(\d{2})(\d{4})$/
 const handleInput = (event: Event) => {
-  const inputElement = event.target as HTMLInputElement
-  let value = inputElement.value.replace(/\D/g, '') // Remove non-numeric characters
-  const regEx =
-    mask.value === 'en-US'
-      ? /^(0[1-9]|1[0-2])\/([1-9]|[12][0-9]|3[01])\/(?!00)\d{4}$/
-      : /^([1-9]|[12][0-9]|3[01])\/([1-9]|1[0-2])\/(?!00)\d{4}$/
-  const formattedValue = value.replace(regEx, '$1/$2/$3') // Apply date format (DD/MM/DD)
-
-  if (value.length >= 10) {
-    value = value.slice(0, 8) // Limit input to 8 characters (YYYY/MM/DD)
-    formattedDate.value = `${value.slice(0, 2)}/${value.slice(2, 4)}/${value.slice(6)}`
-    console.log(formattedDate.value)
-    console.log(value.slice(6))
-
-    inputElement.value = formattedDate.value // Update input with
-  }
-
-  if (value.length === 10) {
-    const parsedDate = dayjs(value, 'YYYY/MM/DD')
-    if (parsedDate.isValid() && parsedDate.isAfter(minDate) && parsedDate.isBefore(maxDate)) {
-      inputElement.value = formattedValue
-      emit('update:modelValue', format(value))
-    } else {
-      inputElement.value = '' // Reset input if date is invalid or out of range
+  const inputEl = event.target as HTMLInputElement
+  inputEl.value = inputEl.value.replace(/\D/g, '')
+  const formattedValue = inputEl.value.replace(regEx, '$1/$2/$3')
+  const checkDate = dayjs(formattedValue, mask.value)
+  if (formattedValue.length === 10 && checkDate.isValid()) {
+    if (checkDate.isValid() && checkDate.isBefore(maxDate) && checkDate.isAfter(minDate)) {
+      inputEl.value = formattedValue
+      emit('update:modelValue', format(formattedValue))
     }
-  } else {
-    inputElement.value = formattedValue // Update input with formatted value
   }
 }
 </script>
