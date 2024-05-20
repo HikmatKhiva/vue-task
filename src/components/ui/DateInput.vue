84e2a423798e3e98295d2c27b62e834f09e8040b
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2024-05-20 20:03:57 +0500

    Update DateInput.vue

--- src/components/ui/DateInput.vue
+++ src/components/ui/DateInput.vue
@@ -1,11 +1,13 @@
 <!-- Using iMask library  -->
- <template>
+<template>
   <input ref="inputRef" v-model="formattedDate" @input="handleInput" :placeholder="mask" />
 </template>
+
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import IMask from 'imask'
 import dayjs from 'dayjs'
+
 defineProps({
   modelValue: {
     type: String,
@@ -35,7 +37,6 @@ onMounted(() => {
 const format = (date: string) => {
   const parsedDate = dayjs(date, 'YYYY-MM-DD')
   return parsedDate.isValid() ? parsedDate.format('YYYY/MM/DD') : undefined
-
 }
 const handleInput = (event: Event) => {
   const inputElement = event.target as HTMLInputElement
@@ -43,6 +44,7 @@ const handleInput = (event: Event) => {
   emit('update:modelValue', date)
 }
 </script> 
+
 <!-- <template>
   <input ref="inputRef" v-model="formattedDate" @input="handleInput" :placeholder="mask" />
 </template>
@@ -83,8 +85,9 @@ const handleInput = (event: Event) => {
   inputElement.value = formattedValue
   emit('update:modelValue', format(value))
 }
-</script> 
+</script>
 <!-- Custom Mask -->
+
 <!-- <template>
   <input
     ref="inputRef"
@@ -93,26 +96,16 @@ const handleInput = (event: Event) => {
     v-model="formattedDate"
     @input="handleInput"
     :placeholder="mask"
-    test="salom"
-    :aria-errormessage="errorMessage"
   />
 </template>
 <style scoped>
 input {
   padding: 5px;
-  outline: none;
-  position: relative;
-}
-input::before {
-  content: "attr(aria-errormessage)";
-  position: absolute;
-  left: 0;
 }
 </style>
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import dayjs from 'dayjs'
-
 const REGEX = /^(\d{2})(\d{2})(\d{4})$/
 
 defineProps({
@@ -122,88 +115,47 @@ defineProps({
     default: ''
   }
 })
-
 const emit = defineEmits(['update:modelValue'])
-const errorMessage = ref<string>('asd')
 const inputRef = ref<HTMLInputElement | null>(null)
 const mask = ref<string>('')
 const formattedDate = ref<string>('')
-
 onMounted(() => {
   const locale = navigator.language || 'en-US'
   mask.value = locale === 'en-US' ? 'MM/DD/YYYY' : 'DD/MM/YYYY'
 })
-
 const minDate = dayjs('1900-01-01', 'YYYY-MM-DD')
 const maxDate = dayjs(new Date().getFullYear() + '-12-31', 'YYYY-MM-DD')
-
 const format = (date: string) => {
   const parsedDate = dayjs(date, 'YYYY-MM-DD')
   return parsedDate.isValid() ? parsedDate.format('YYYY/MM/DD') : ''
 }
-
 const handleInput = (event: Event) => {
   const inputEl = event.target as HTMLInputElement
-  if (inputEl.value.length > 10) return
+  const formattedValue = inputEl.value.replace(REGEX, '$1/$2/$3')
   inputEl.value = inputEl.value.replace(/\D/g, '')
-  const validValue = isDateValid(inputEl.value)
-  const formattedValue = validValue.replace(REGEX, '$1/$2/$3')
   const checkDate = dayjs(formattedValue, mask.value)
-  if (
-    formattedValue.length == 10 &&
-    checkDate.isValid() &&
-    checkDate.isBefore(maxDate) &&
-    checkDate.isAfter(minDate)
-  ) {
-    console.log(formattedValue)
-    inputEl.style.borderColor = ''
-    inputEl.value = formattedValue
-  } else {
-    inputEl.ariaInvalid = 'invalid date time'
-    inputEl.style.borderColor = 'red'
-  }
+  inputEl.value = dateValidate(inputEl.value)
 
-  // if (
-  //   formattedValue.length === 10 &&
-  //   checkDate.isValid() &&
-  //   checkDate.isBefore(maxDate) &&
-  //   checkDate.isAfter(minDate)
-  // ) {
-  //   inputEl.value = formattedValue
-  //   // inputEl.style.borderColor = 'inherit'
-  //   emit('update:modelValue', format(formattedValue))
-  // }
+  if (mask.value === 'MM/DD/YYYY') {
+    // parseInt(inputEl.value.slice(0, 2)) > 12
+  }
+  if (formattedValue.length === 10) {
+    if (checkDate.isValid() && checkDate.isBefore(maxDate) && checkDate.isAfter(minDate)) {
+      emit('update:modelValue', format(formattedValue))
+    }
+  }
 }
 
-function isDateValid(date: string): string {
-  const minYear = 1900
-  const maxYear = new Date().getFullYear()
-  const m = date.slice(0, 2)
-  const d = date.slice(2, 4)
-  const y = date.slice(4, 8)
-  // console.log(dayjs(m,'month'));
-  let ttt = dayjs()
-  const isValidMonth = dayjs(m, 'MM', true)
-  console.log(isValidMonth)
+function dateValidate(value: string): string {
+  const minDate = 1990
+  const maxDate = new Date().getFullYear() + 1
+  let dateValue: string = value
 
-  let month: string = ''
-  let day: string = ''
-  let year: string = ''
+  const month = value.slice(0, 2)
+  const day = value.slice(2, 4)
+  const year = value.slice(4, 8)
+  console.log(+month < 12)
 
-  // if (parseInt(m) <= 12 && parseInt(m) > 0) {
-  //   month = parseInt(m) < 10 ? '0' + m : m
-  // }
-
-  if (parseInt(d) <= 31 && parseInt(d) > 0) {
-    day = parseInt(d) < 10 ? '0' + d : d
-  }
-
-  if (minYear <= parseInt(y) && maxYear >= parseInt(y)) {
-    year = y
-  } else {
-    year = ''
-  }
-
-  return `${month}${day}${year}`
+  return dateValue
 }
 </script> -->
