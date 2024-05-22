32141859a1e8b772f31b0a0941c3bb3e50a5e78d
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2024-05-22 11:47:59 +0500

    update

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ const date = ref<string>('')
 </script>
 <template>
   <div class="center">
-    <input type="text" v-mask-date>
+    <input type="text" v-mask-date="date" v-model="date">
     <!-- <DateInput v-model="date" /> -->
   </div>
 </template>
--- src/directives/date-mask.ts
+++ src/directives/date-mask.ts
@@ -1,20 +1,20 @@
-import type { Directive } from 'vue'
+import type { Directive, DirectiveBinding } from 'vue'
 import dayjs from 'dayjs'
 const maskDate: Directive = {
-  mounted(el: HTMLElement) {
+  mounted(el: HTMLElement, binding: DirectiveBinding) {
+    console.log(binding.value);
     const inputEl: HTMLInputElement = el as HTMLInputElement
+    inputEl.maxLength = 8
     const locale = (navigator.language || 'en-US') === 'en-US' ? 'MM/DD/YYYY' : 'DD/MM/YYYY'
     inputEl.placeholder = locale
     inputEl.addEventListener('input', () => {
       const value = isValid(inputEl.value, locale)
       inputEl.value = value
     })
-
     function isValid(date: string, mask: string): string {
-      const minYear = 1990
-      const maxYear = 2020
       const REGEX = /^(\d{2})(\d{2})(\d{4})$/
       date = date.replace(/\D/g, '')
+      inputEl.classList.remove('error')
       const formattedValue = inputEl.value.replace(REGEX, '$1/$2/$3')
       const month = dayjs(date.slice(0, 2)).month()
       const day = dayjs(date.slice(2, 4)).day()
@@ -30,18 +30,34 @@ const maskDate: Directive = {
       if (parseInt(date[4]) < 1 || parseInt(date[4]) > 2) {
         date = date.slice(0, 4) + ''
       }
-      if (date.length === 8) {
-        date = formattedValue
+      if (parseInt(date[4]) > 1 && parseInt(date[5]) > 0) {
+        date = date.slice(0, 5)
       }
-      if (
-        dayjs(formattedValue, mask).isValid() &&
-        dayjs(formattedValue).isAfter(minYear) &&
-        dayjs(formattedValue).isBefore(maxYear)
-      ) {
-        date = formattedValue
+      if (parseInt(date[4]) > 1 && parseInt(date[6]) > 2) {
+        date = date.slice(0, 6)
+      }
+      if (parseInt(date[4]) > 1 && parseInt(date[6]) > 1 && parseInt(date[7]) > 0) {
+        date = date.slice(0, 7)
+      }
+      if (date.length === 8 && dayjs(formattedValue, mask, true).isValid()) {
+        date = dayjs(formattedValue).format(mask)
       }
       return date
     }
+  },
+  updated(el: HTMLElement, binding: DirectiveBinding)  {
+    const inputEl: HTMLInputElement = el as HTMLInputElement
+    const locale = (navigator.language || 'en-US') === 'en-US' ? 'MM/DD/YYYY' : 'DD/MM/YYYY'
+    const REGEX = /^(\d{2})(\d{2})(\d{4})$/
+    const formattedValue = inputEl.value.replace(REGEX, '$1/$2/$3')
+    if(inputEl.value.length === 8 ) {
+      const formatted = dayjs(formattedValue).format(locale)
+      const parsed = dayjs(formatted,'YYYY-MM-DD')
+      parsed.isValid() ? parsed.format('YYYY/MM/DD') : undefined
+      console.log( parsed.isValid() ? parsed.format('YYYY/MM/DD') : undefined);
+      binding.value = parsed.isValid() ? parsed.format('YYYY/MM/DD') : undefined
+    }
   }
+  
 }
 export default maskDate
